123742738c979dc643429fd6f2d95dd2c04711f3
isa <isaazobu@gmail.com>
2020-05-06 01:56:56 +0300

    Spesific Error Handling

--- controllers/coders.js
+++ controllers/coders.js
@@ -1,93 +1,77 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 const Coder = require("../models/Coders");
 
 // @desc Get all coders
 // @route GET /api/v1/coders
 // @access Public
-getCoders = async (req, res, next) => {
-  try {
-    const coders = await Coder.find();
-    res.status(200).json({
-      success: true,
-      count: coders.length,
-      data: coders,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err });
-  }
-};
+getCoders = asyncHandler(async (req, res, next) => {
+  const coders = await Coder.find();
+  res.status(200).json({
+    success: true,
+    count: coders.length,
+    data: coders,
+  });
+});
 
 // @desc Get single coders
 // @route GET /api/v1/coders/:id
 // @access Public
-getCoder = async (req, res, next) => {
-  try {
-    const coder = await Coder.findById(req.params.id);
-    if (!coder) {
-      return res.status(400).json({ success: false });
-    }
-    res.status(200).json({
-      success: true,
-      data: coder,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err });
+getCoder = asyncHandler(async (req, res, next) => {
+  const coder = await Coder.findById(req.params.id);
+  if (!coder) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({
+    success: true,
+    data: coder,
+  });
+});
 // @desc Create new coder
 // @route POST /api/v1/coders
 // @access Private
-createCoder = async (req, res, next) => {
-  try {
-    const coder = await Coder.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: coder,
-    });
-  } catch (err) {
-    res.status(400).json({ succes: false });
-  }
-};
+createCoder = asyncHandler(async (req, res, next) => {
+  const coder = await Coder.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: coder,
+  });
+});
 // @desc Update coder
 // @route PUT /api/v1/coders/:id
 // @access Private
-updateCoder = async (req, res, next) => {
-  try {
-    coder = await Coder.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!coder) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Coder is not exist" });
-    }
-    res.status(200).json({
-      success: true,
-      data: coder,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err });
+updateCoder = asyncHandler(async (req, res, next) => {
+  coder = await Coder.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!coder) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({
+    success: true,
+    data: coder,
+  });
+});
 // @desc Delete coder
 // @route DELETE /api/v1/coders/:id
 // @access Private
-deleteCoder = async (req, res, next) => {
-  try {
-    coder = await Coder.findByIdAndDelete(req.params.id);
-    if (!coder) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Coder is not exist" });
-    }
-    res.status(204).json({
-      success: true,
-      data: {},
-    });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err });
+deleteCoder = asyncHandler(async (req, res, next) => {
+  coder = await Coder.findByIdAndDelete(req.params.id);
+  if (!coder) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(204).json({
+    success: true,
+    data: {},
+  });
+});
 
 module.exports = {
   getCoders,
