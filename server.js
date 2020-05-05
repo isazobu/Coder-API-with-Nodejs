917a8c2816f5f282968a01b23056d93eaa3a33cd
isa <isaazobu@gmail.com>
2020-05-05 18:24:07 +0300

    Added MongoDB Integration

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan');
-
-// Route files
-const coders = require("./routes/coders");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+// Route files
+const coders = require("./routes/coders");
+
 const app = express();
 
 // Dev loggin middleware
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-
 
 // Mount routers
 app.use("/api/v1/coders", coders);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server is up and runnning in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
+
+// Handle unhandled rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
